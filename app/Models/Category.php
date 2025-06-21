0312a89b2f6302c3b0c2260df384548fa317b968
desmoris <damianesmoris@gmail.com>
2025-06-21 17:26:53 -0300

    Se añaden los métodos en los modelos para relacionar tareas con categorias

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Task;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     use HasFactory;
     protected $table = 'categories';
     public $timestamps = false;
+    protected $hidden = ['pivot'];
+
+    public function assignedTo(): BelongsToMany
+    {
+        return $this->belongsToMany(Task::class, "have_assigned");
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,12 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Task extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    protected $hidden = ['pivot'];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, "have_assigned");
+    }
 }
