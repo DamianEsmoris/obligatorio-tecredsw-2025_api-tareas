d1c348ddee330e2bb3490bff8ca562c17d680dc2
desmoris <damianesmoris@gmail.com>
2025-06-20 23:07:00 -0300

    Se agregan los endpoints `/task`

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/status', fn() => "OK");
+
+Route::post('/task', [TaskController::class, 'Create']);
+Route::get('/task', [TaskController::class, 'GetAll']);
+Route::get('/task/{d}', [TaskController::class, 'Get']);
+Route::put('/task/{d}', [TaskController::class, 'Modify']);
+Route::delete('/task/{d}', [TaskController::class, 'Delete']);
