ece5faebcdf3f26756f093ce3557666133b29b57
desmoris <damianesmoris@gmail.com>
2025-06-21 15:40:27 -0300

    A ggmartinez se le cayó un script para configurar Laravel

--- docker/php-fpm/start.sh
+++ docker/php-fpm/start.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-composer install
-php artisan key:generate
-php artisan migrate:fresh
-php artisan db:seed
+[ -z "${SKIP_COMPOSER}" ] \
+    && composer install
+
+[ -z "${SKIP_MIGRATIONS}" ] \
+    && php artisan migrate
+
+[ -z "${SKIP_SEEDERS}" ] \
+    && php artisan db:seed
+
+[ -z "${STORAGE_LINK}" ] \
+    && php artisan storage:link
+
+if [ ! -f .env ]
+then
+   cp .env.example .env
+   php artisan key:generate
+fi
 
 php-fpm -F -y /etc/php-fpm.conf
