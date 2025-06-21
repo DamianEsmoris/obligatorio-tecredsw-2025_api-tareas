f872a061a2c8abf4b0626350be84bfdb79c1d272
desmoris <damianesmoris@gmail.com>
2025-06-21 16:42:29 -0300

    Seeder para las categorias pronto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Task;
 
 use Illuminate\Database\Seeder;
@@ -10,6 +11,7 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        Category::factory(30)->create();
         Task::factory(20)->create();
     }
 }
