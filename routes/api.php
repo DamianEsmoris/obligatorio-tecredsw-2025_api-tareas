7f597fe32cce0dc2c57ea1c29d0fa7610637433b
desmoris <damianesmoris@gmail.com>
2025-06-22 14:34:34 -0300

    Se añaden las rutas CRUD para los comentarios

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ Route::get('/category', [CategoryController::class, 'GetAll']);
 Route::get('/category/{d}', [CategoryController::class, 'Get']);
 Route::put('/category/{d}', [CategoryController::class, 'Modify']);
 Route::delete('/category/{d}', [CategoryController::class, 'Delete']);
+
+Route::post('/comment', [CommentController::class, 'Create']);
+Route::get('/comment', [CommentController::class, 'GetAll']);
+Route::get('/comment/{d}', [CommentController::class, 'Get']);
+Route::put('/comment/{d}', [CommentController::class, 'Modify']);
+Route::delete('/comment/{d}', [CommentController::class, 'Delete']);
