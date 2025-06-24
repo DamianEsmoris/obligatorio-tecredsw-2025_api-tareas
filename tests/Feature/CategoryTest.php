80528fed00d974698523bd3ce57021c259eba8b0
desmoris <damianesmoris@gmail.com>
2025-06-24 02:28:56 -0300

    Pruebas con y sin autemticación

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -3,11 +3,13 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
     use RefreshDatabase;
+    private $accessToken = null;
 
     protected function setUp(): void
     {
@@ -21,6 +23,45 @@ class CategoryTest extends TestCase
             'id',
             'name',
         ];
+    }
+
+    private function getAccessToken($retried = false)
+    {
+        if ($this->accessToken != null)
+            return $this->accessToken;
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ])->post(config('services.api_oauth.login_url'), [
+            "grant_type" => "password",
+            "client_id" => config('services.api_oauth.client_id'),
+            "client_secret" => config('services.api_oauth.client_secret'),
+            "username" => "pruebas@api-tareas",
+            "password" => "%.NU*GTxFL*(~.=K]=\H"
+        ]);
+
+        if (!$response->successful() && !$retried) {
+            Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/json',
+            ])->post(config('services.api_oauth.register_url'), [
+                "name" => "pruebas-api-tareas",
+                "email" => "pruebas@api-tareas",
+                "password" => "%.NU*GTxFL*(~.=K]=\H",
+                "password_confirmation" => "%.NU*GTxFL*(~.=K]=\H"
+            ]);
+            return $this->getAccessToken(true);
+        }
+        $this->accessToken = $response->json('access_token');
+        return $this->accessToken;
+    }
+
+    private function requestWithToken()
+    {
+        $accessToken = $this->getAccessToken();
+        return $this->withHeaders([
+            'Authorization' => 'Bearer ' . $this->getAccessToken(),
+        ]);
     }
 
     private function getCategoryStructureWithTasks() : Array
@@ -39,9 +80,11 @@ class CategoryTest extends TestCase
         ];
     }
 
+    // GET: /api/category
+
     public function test_getAllCategories(): void
     {
-        $response = $this->get('/api/category');
+        $response = $this->getJson('/api/category');
         $response->assertStatus(200);
         $response->assertJsonStructure([
             '*' => $this->getCategoryStructureWithTasks()
@@ -50,67 +93,100 @@ class CategoryTest extends TestCase
 
     public function test_getExistentCategory(): void
     {
-        $response = $this->get('/api/category/1');
+        $response = $this->getJson('/api/category/1');
         $response->assertStatus(200);
         $response->assertJsonStructure($this->getCategoryStructureWithTasks());
     }
 
     public function test_getNoneExistentCategory(): void
     {
-        $response = $this->get('/api/category/999');
+        $response = $this->getJson('/api/category/999');
         $response->assertStatus(404);
     }
 
-    public function test_createCategory(): void
+    // POST: /api/category
+
+    public function test_createCategoryNoAuth(): void
     {
-        $response = $this->post('/api/category', $this->getExampleCategory());
+        $response = $this->postJson('/api/category', $this->getExampleCategory());
+        $response->assertStatus(401);
+    }
+
+    public function test_createCategoryAuth(): void
+    {
+        $categoryData = $this->getExampleCategory();
+        $response = $this->requestWithToken()->postJson('/api/category', $categoryData);
+
         $response->assertStatus(201);
         $response->assertJsonStructure($this->getBasicCategoryStructure());
+
         $data = $response->json();
         $this->assertDatabaseHas('categories', [
-            'id' => $data['id']
+            'id' => $data['id'],
+            'name' => $categoryData['name'],
         ]);
     }
 
-    public function test_updateExistentCategory(): void
+    // PUT: /api/category/{id}
+
+    public function test_updateExistentCategoryNoAuth(): void
     {
-        $response = $this->put('/api/category/1', $this->getExampleCategory());
+        $response = $this->putJson('/api/category/1', $this->getExampleCategory());
+        $response->assertStatus(401);
+    }
+
+    public function test_updateExistentCategoryAuth(): void
+    {
+        $updatedData = ['name' => 'Updated Category Name'];
+        $response = $this->requestWithToken()->putJson('/api/category/1', $updatedData);
+
         $response->assertStatus(200);
         $response->assertJsonStructure($this->getBasicCategoryStructure());
-        $data = $response->json();
         $this->assertDatabaseHas('categories', [
-            ...$this->getExampleCategory(),
-            'id' => $data['id']
+            'id' => 1,
+            'name' => 'Updated Category Name',
         ]);
     }
 
-    public function test_updateNoneExistentCategory(): void
+    public function test_updateNoneExistentCategoryNoAuth(): void
     {
-        $response = $this->put('/api/category/999', $this->getExampleCategory());
+        $response = $this->putJson('/api/category/999', $this->getExampleCategory());
+        $response->assertStatus(401);
+    }
+
+    public function test_updateNoneExistentCategoryAuth(): void
+    {
+        $response = $this->requestWithToken()->putJson('/api/category/999', $this->getExampleCategory());
         $response->assertStatus(404);
-        $this->assertDatabaseMissing('categories', [
-            'id' => 999,
-            'deleted_at' => null
-        ]);
     }
 
-    public function test_deleteExistentCategory(): void
+    // DELETE: /api/category/{id}
+
+    public function test_deleteExistentCategoryNoAuth(): void
     {
-        $response = $this->delete('/api/category/2');
+        $response = $this->deleteJson('/api/category/2');
+        $response->assertStatus(401);
+    }
+
+    public function test_deleteExistentCategoryAuth(): void
+    {
+        $response = $this->requestWithToken()->deleteJson('/api/category/2');
         $response->assertStatus(200);
-        $this->assertDatabaseMissing('categories', [
+        $this->assertSoftDeleted('categories', [
             'id' => 2,
-            'deleted_at' => null
         ]);
     }
 
-    public function test_deleteNoneExistentCategory(): void
+    public function test_deleteNoneExistentCategoryNoAuth(): void
     {
-        $response = $this->delete('/api/category/999');
-        $response->assertStatus(404);
-        $this->assertDatabaseMissing('categories', [
-            'id' => 999,
-            'deleted_at' => null
-        ]);
+        $response = $this->deleteJson('/api/category/999');
+        $response->assertStatus(401);
     }
+
+    public function test_deleteNoneExistentCategoryAuth(): void
+    {
+        $response = $this->requestWithToken()->deleteJson('/api/category/999');
+        $response->assertStatus(404);
+    }
+
 }
