dadd3648a666ba6c1ed5f03b437732134e5a05fa
desmoris <damianesmoris@gmail.com>
2025-06-24 02:29:41 -0300

    Correciones y verificaciones adicionales post pruebas

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,10 +6,13 @@ use App\Models\Task;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
     protected $table = 'categories';
     public $timestamps = false;
     protected $hidden = ['pivot'];
